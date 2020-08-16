2e6481d70f0d097f560e3fde8ea4e6372443ffb4
Anis <Anis1011@gmail.com>
2020-08-16 16:54:47 -0700

    first commit

---
+++ build/precache-manifest.c27ed5bc0e33a86eeb403f93150ea397.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "eb0eeb6ef64a3f9ba16a82fa9a53d70e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7696182d39d244555712",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "8f35094db4f09810df42",
+    "url": "/static/js/2.41083c5f.chunk.js"
+  },
+  {
+    "revision": "8ebb70227fec3b61ad3af3d99ebe8940",
+    "url": "/static/js/2.41083c5f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7696182d39d244555712",
+    "url": "/static/js/main.f8cb23d9.chunk.js"
+  },
+  {
+    "revision": "66c82a433ffffce6fc2c",
+    "url": "/static/js/runtime-main.134086db.js"
+  }
+]);
